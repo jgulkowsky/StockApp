7345edccc3da1924d8b7952daff1fc238734cda5
jgulkowsky <jgulkowsky@gmail.com>
2023-12-15 13:45:21 +0100

    project init init / add .gitignore / remove storyboards

---
+++ StockApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  StockApp
+//
+//  Created by Jan Gulkowski on 15/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
