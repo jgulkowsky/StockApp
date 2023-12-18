9c9e986207bcaea4bc5347a31a190c4935e3ff3b
jgulkowsky <jgulkowsky@gmail.com>
2023-12-18 20:30:39 +0100

    add DGCharts for rendering the CandleStickCharts and show a basic chart in the VC

--- StockApp/QuoteScreen/QuoteViewController.swift
+++ StockApp/QuoteScreen/QuoteViewController.swift
@@ -8,9 +8,16 @@
 import UIKit
 import SnapKit
 import Combine
+import DGCharts
 
 class QuoteViewController: UIViewController {
     private var viewModel: QuoteViewModel
+    
+    private lazy var chartView: CandleStickChartView = {
+        let chartView = CandleStickChartView()
+        chartView.backgroundColor = .purple
+        return chartView
+    }()
     
     private lazy var bidPriceLabel = UILabel(frame: .zero)
     private lazy var askPriceLabel = UILabel(frame: .zero)
@@ -31,19 +38,36 @@ class QuoteViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        view.addSubview(bidPriceLabel)
-        view.addSubview(askPriceLabel)
-        view.addSubview(lastPriceLabel)
-        
+        addViews()
         setupConstraints()
         setupBindings()
     }
 }
 
+extension QuoteViewController: ChartViewDelegate {
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        print("@jgu: chartValueSelected: \(entry)")
+    }
+}
+
 private extension QuoteViewController {
+    func addViews() {
+        view.addSubview(chartView)
+        view.addSubview(bidPriceLabel)
+        view.addSubview(askPriceLabel)
+        view.addSubview(lastPriceLabel)
+    }
+    
     func setupConstraints() {
-        bidPriceLabel.snp.makeConstraints { (make) -> Void in
+        chartView.snp.makeConstraints { (make) -> Void in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).inset(20.0)
+            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).inset(20.0)
+            make.height.equalTo(100.0)
+        }
+        
+        bidPriceLabel.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(chartView.snp.bottom)
             make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).inset(20.0)
             make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).inset(20.0)
             make.height.equalTo(30.0)
@@ -65,17 +89,38 @@ private extension QuoteViewController {
     }
     
     func setupBindings() {
+        viewModel.chartDataPublisher
+            .receive(on: RunLoop.main)
+            .sink { self.setChartView(basedOn: $0) }
+            .store(in: &store)
+        
         viewModel.bidPricePublisher
             .receive(on: RunLoop.main)
             .sink { self.bidPriceLabel.text = $0 }
             .store(in: &store)
+        
         viewModel.askPricePublisher
             .receive(on: RunLoop.main)
             .sink { self.askPriceLabel.text = $0 }
             .store(in: &store)
+        
         viewModel.lastPricePublisher
             .receive(on: RunLoop.main)
             .sink { self.lastPriceLabel.text = $0 }
             .store(in: &store)
+    }
+    
+    func setChartView(basedOn chartData: ChartData) {
+        let entries = chartData.values.map {
+            CandleChartDataEntry(
+                x: $0.date.timeIntervalSince1970,
+                shadowH: $0.high,
+                shadowL: $0.low,
+                open: $0.open,
+                close: $0.close)
+        }
+        let set = CandleChartDataSet(entries: entries, label: "Chart chart chart")
+        let data = CandleChartData(dataSet: set)
+        chartView.data = data
     }
 }
--- StockApp/QuoteScreen/QuoteViewModel.swift
+++ StockApp/QuoteScreen/QuoteViewModel.swift
@@ -9,6 +9,11 @@ import Foundation
 import Combine
 
 class QuoteViewModel {
+    var chartDataPublisher: AnyPublisher<ChartData, Never> {
+        chartDataSubject
+            .eraseToAnyPublisher()
+    }
+    
     var bidPricePublisher: AnyPublisher<String, Never> {
         bidPriceSubject
             .map { value in
@@ -45,6 +50,7 @@ class QuoteViewModel {
             .eraseToAnyPublisher()
     }
     
+    private var chartDataSubject = CurrentValueSubject<ChartData, Never>(ChartData(values: []))
     private var bidPriceSubject = CurrentValueSubject<Double?, Never>(nil)
     private var askPriceSubject = CurrentValueSubject<Double?, Never>(nil)
     private var lastPriceSubject = CurrentValueSubject<Double?, Never>(nil)
@@ -59,6 +65,17 @@ class QuoteViewModel {
 private extension QuoteViewModel {
     func fetchData() async throws {
         try? await Task.sleep(nanoseconds: 1 * 1_000_000_000) // 1 second
+        
+        let chartData = ChartData(
+            values: [
+                ChartItem(close: 90.0, high: 100.0, low: 70.0, open: 80.0, date: Date(timeIntervalSince1970: 1702911600)),
+                ChartItem(close: 100.0, high: 110.0, low: 50.0, open: 80.0, date: Date(timeIntervalSince1970: 1702915200)),
+                ChartItem(close: 90.0, high: 150.0, low: 20.0, open: 80.0, date: Date(timeIntervalSince1970: 1702918800)),
+                ChartItem(close: 90.0, high: 90.0, low: 10.0, open: 10.0, date: Date(timeIntervalSince1970: 1702922400)),
+                ChartItem(close: 90.0, high: 140.0, low: 70.0, open: 80.0, date: Date(timeIntervalSince1970: 1702926000))
+            ]
+        )
+        chartDataSubject.send(chartData)
         
         let quote = Quote(date: .now, bidPrice: 171.12, askPrice: 171.33, lastPrice: 171.12)
         bidPriceSubject.send(quote.bidPrice)
