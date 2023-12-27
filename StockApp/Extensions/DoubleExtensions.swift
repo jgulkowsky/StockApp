f8040fd74f16bd56079918fe5147ed476b550e41
jgulkowsky <jgulkowsky@gmail.com>
2023-12-27 18:37:53 +0100

    add todo

--- StockApp/Extensions/DoubleExtensions.swift
+++ StockApp/Extensions/DoubleExtensions.swift
@@ -9,6 +9,6 @@ import Foundation
 
 extension Double {
     func to2DecPlaces() -> String {
-        return String(format: "%.2f", self)
+        return String(format: "%.2f", self) // todo: in fact we should also round the value as now we are lying a little bit to the user - let's add tests for this and update this method to the proper one
     }
 }
