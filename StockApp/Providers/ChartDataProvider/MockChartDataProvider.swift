4075d9b9a5845c06a14d842b12a4e565ce6f3a93
jgulkowsky <jgulkowsky@gmail.com>
2024-01-02 12:56:55 +0100

    add tests for DateExtensions / remove not used daysInCurrentMonth / refactor last30Days function - so it can be testable - as static version couldn't be

--- StockApp/Providers/ChartDataProvider/MockChartDataProvider.swift
+++ StockApp/Providers/ChartDataProvider/MockChartDataProvider.swift
@@ -10,17 +10,11 @@ import Foundation
 class MockChartDataProvider: ChartDataProviding {
     func getChartData(forSymbol symbol: String) async throws -> ChartData {
         try? await Task.sleep(nanoseconds: 2 * 1_000_000_000) // 2 seconds
-        let dates = Date.last30Days()
-    //        let dates = Date.daysInCurrentMonth()
-    //        let dates = Date.daysBetween(
-    //            startDate: Date(timeIntervalSince1970: 1698796800), // 1st of November
-    //            andEndDate: Date(timeIntervalSince1970: 1701388800) // 1st of December
-    //        )
-    //        let dates = Date.daysBetween(
-    //            startDate: Date(timeIntervalSince1970: 1698796800), // 1st of November
-    //            andEndDate: Date(timeIntervalSince1970: 1699142400) // 5th of November
-    //        )
-        
+//        let dates = Date.last30Days()
+        let dates = Date.daysBetween(
+            startDate: Date(timeIntervalSince1970: 1698796800), // 1st of November
+            andEndDate: Date(timeIntervalSince1970: 1701388800) // 1st of December
+        )
         let items = generateRandomChartItems(for: dates)
         let chartData = ChartData(values: items)
         return chartData
